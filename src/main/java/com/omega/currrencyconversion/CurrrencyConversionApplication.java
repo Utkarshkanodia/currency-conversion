cd21e5856f3b9d7f0c218372917bfd0e1c8713ef
Utkarsh Kanodia <utkarshkanodia22@outlook.com>
2024-03-11 18:03:31 +0530

    first commit

---
+++ src/main/java/com/omega/currrencyconversion/CurrrencyConversionApplication.java
@@ -0,0 +1,13 @@
+package com.omega.currrencyconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrrencyConversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrrencyConversionApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/omega/currrencyconversion/controller/CurrencyConversionController.java
@@ -0,0 +1,19 @@
+package com.omega.currrencyconversion.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyConversionController {
+
+    // @GetMapping("/latest")
+    // public  getConversion(@RequestParam(name = "base_currency") String baseCurrency,
+    //         @RequestParam(name = "currencies") List<String> currencies) {
+                
+                
+    // }
+
+}
